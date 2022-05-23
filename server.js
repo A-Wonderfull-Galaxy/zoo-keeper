48f4e92e2724a5bde5c6de9daaf8b8de75f8ed70
A-Wonderfull-Galaxy <marlonq16@gmail.com>
2022-05-23 17:44:51 -0400

    MVP feature

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const {animals}= require('./data/animals')
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const {animals}= require('./data/animals');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static('public'));
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray){
     let personalityTraitsArray =[];
@@ -18,7 +27,6 @@ function filterByQuery(query, animalsArray){
             personalityTraitsArray = query.personalityTraits;
         }
     
-
         //loop through each trait in the personalitytraits array:
         personalityTraitsArray.forEach(trait =>{
             // Check the trait against each animal in the filteredResults array.
@@ -45,19 +53,94 @@ function filterByQuery(query, animalsArray){
     }
     //return the filtered results:
     return filteredResults;
+};
+
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+
+    return result;
 }
 
-app.get('/api/animals', (req,res) =>{
+function createNewAnimal(body, animalsArray){
+    const animal = body;
+
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__direname, './data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+}
+
+app.get('/api/animals', (req, res) => {
     let results = animals;
 
-    if (req.query){
-        results = filterByQuery(req.query, results);
+    if (req.query) {
+      results = filterByQuery(req.query, results);
     }
 
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now port 3001!`);
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+
+    if (result) {
+      res.json(result);
+    }else{
+      res.send(404);
+    }
 });
 
+app.post('/api/animals', (req,res) => {
+    //set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    //add animal to json file and animals array in this function
+    if (!validateAnimal(req.body)){
+        res.status(400).send('The animal is not properly formatted.')
+    }else{
+        const animal = createNewAnimal(req.body, animals);
+
+        res.json(animal);
+    }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeeprs', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/zookeeper.html'));
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
+  
